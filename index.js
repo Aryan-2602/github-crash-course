e6413e562bb4521b89ca524d4defcc37dbb9dd36
Aryan Maheshwari <aryan26.03.02@gmail.com>
2024-03-30 00:11:34 +0530

    revert something

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ function sayName(){
     console.log('My Name is ' + name1)
 }
 
+<<<<<<< HEAD
 function newFn(){
     console.log("Hey i am a new fn")
 }
 
 
+=======
+function isAbove18(){
+    return age >=18
+}
+>>>>>>> parent of d56d161 (Remove 18 fn)
 
