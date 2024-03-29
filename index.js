08f6e8d804baf47f69af182e9612f60cfec4d779
Aryan Maheshwari <aryan26.03.02@gmail.com>
2024-03-29 18:41:08 +0530

    Added isAbove18 fn

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const age = 24;
 
-const name = 'Aryan'
+const name1 = 'Aryan'
 
 const dob = '26/03/2002'
 
 function sayName(){
-    console.log('My Name is ' + name)
+    console.log('My Name is ' + name1)
 }
+
+function isAbove18(){
+    return age >=18
+}
+
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 function sayhello() {
     console.log("Hello")
 }
+
