0d6a7af8f93449f78374097ed84b970dae6d0b5c
Aryan Maheshwari <aryan26.03.02@gmail.com>
2024-03-30 15:42:19 +0530

    new feature added

--- index.js
+++ index.js
@@ -8,15 +8,17 @@ function sayName(){
     console.log('My Name is ' + name1)
 }
 
-<<<<<<< HEAD
+
 function newFn(){
     console.log("Hey i am a new fn")
 }
 
 
-=======
+
 function isAbove18(){
     return age >=18
 }
->>>>>>> parent of d56d161 (Remove 18 fn)
 
+function new_featureAdd(){
+    console.log('this is a new feature')
+}
